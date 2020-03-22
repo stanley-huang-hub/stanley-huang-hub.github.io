ab83d6db87642b3703dd40ebb12a4613b6465780
stanley-huang-hub <stanleyh17@gmail.com>
2020-03-22 16:22:13 -0400

    added section headers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 
-
+// (function ($) {
+//     // Smooth scrolling using jQuery easing
+//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+//       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//         let target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//           $('html, body').animate({
+//             scrollTop: (target.offset().top)
+//           }, 1000, "easeInOutExpo");
+//           return false;
+//         }
+//       }
+//     });
+  
+//     // Close responsive menu on trigger
+//     $('.js-scroll-trigger').click(function () {
+//       $('.navbar-collapse').collapse('hide');
+//     });
+  
+//     // Add active class to navbar items on scroll
+//     $('body').scrollspy({
+//       target: '#navbarSupportedContent'
+//     });
+//   })(jQuery);
 
